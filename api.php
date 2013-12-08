1555d836113c7fcbdba34176aed76279cf3234b4
root <root@localhost.localdomain>
2013-12-08 22:46:02 +0900

    hoge

--- api.php
+++ api.php
@@ -1,22 +1,38 @@
 <?php
-
-  header("Content-Type: application/json; charset=utf-8");
-
-  $response = array();
-
   switch ($_SERVER['REQUEST_URI']) {
-    case '/api/hello':
-      $response['data'] = 'world';
+    case '/api/item': case '/api/item.json':
+      header("Content-Type: application/json; charset=utf-8");
+      $response = array();
+      $file     = './item.csv';
+      $fp       = fopen($file, "r");
+      while (($data = fgetcsv($fp, 0, ",")) !== FALSE) {
+        $response[] = $data;
+      }
+      fclose($fp);
+      echo json_encode($response);
       break;
-    case '/api/item':
-      $response['data'] = array(
-        'id'          => 100,
-        'title'       => 'やばい商品',
-        'price'       => 5000,
-        'description' => 'これはまじでやばい'
-      );
+    case '/api/item.xml':
+      header("Content-Type: application/xml; charset=utf-8");
+      $response = array();
+      $file     = './item.csv';
+      $fp       = fopen($file, "r");
+      while (($data = fgetcsv($fp, 0, ",")) !== FALSE) {
+        $response[] = $data;
+      }
+      fclose($fp);
+      $xml    = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
+      $xml   .= "<items>\n";
+      foreach ($response as $value) {
+        $xml .= "<item>\n";
+        $xml .= "<ItemID>" . $value['0'] . "</ItemID>";
+        $xml .= "<CategoryID>" . $value['1'] . "</CategoryID>";
+        $xml .= "<ItemName>" . $value['2'] . "</ItemName>";
+        $xml .= "<Price>" . $value['3'] . "</Price>";
+        $xml .= "</item>";
+      }
+      $xml .= "</items>\n";
+      echo $xml;
       break;
   }
 
-  echo json_encode($response);
 ?>
