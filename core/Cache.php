db2d9d5234856da952b25f6ea9b9bc8c36299b40
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-02-18 15:16:16 +0000

    memcachedのクラス作成

--- core/Cache.php
+++ core/Cache.php
@@ -9,6 +9,26 @@ namespace Core;
 */
 class Cache
 {
+    public function __construct()
+    {
+        $memcached = new Memcached();
+        $memcached->addServer('localhost', 11211);
+    }
 
+    public function set($key, $data)
+    {
+        $result = $memcached->set($key, $data, time() + 60);
+        if (! $result){
+            return false;
+        }
 
+        return ture;
+    }
+
+    public function get($key)
+    {
+        $result = $memcached->get($key);
+      
+        return $result;
+    }
 }
